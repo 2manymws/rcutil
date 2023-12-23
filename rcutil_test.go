a7d0aa367452569bd379bc96096ebd92b88e9944
k1LoW <k1lowxb@gmail.com>
2023-12-23 21:36:23 +0900

    Set error for Seed()

--- rcutil_test.go
+++ rcutil_test.go
@@ -13,6 +13,78 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
+func TestSeed(t *testing.T) {
+	tests := []struct {
+		req     *http.Request
+		vary    []string
+		want    string
+		wantErr bool
+	}{
+		{nil, nil, "", true},
+		{
+			&http.Request{
+				Method: http.MethodGet,
+				Host:   "example.com",
+			},
+			nil,
+			"",
+			true,
+		},
+		{
+			&http.Request{
+				Method: http.MethodGet,
+				Host:   "example.com",
+				URL: &url.URL{
+					Host: "example.com",
+					Path: "/foo",
+				},
+			},
+			nil,
+			"get|example.com|example.com|/foo|",
+			false,
+		},
+		{
+			&http.Request{
+				Method: http.MethodPost,
+				Host:   "example.com",
+				URL: &url.URL{
+					Host: "127.0.0.1",
+					Path: "/foo",
+				},
+			},
+			nil,
+			"post|example.com|127.0.0.1|/foo|",
+			false,
+		},
+		{
+			&http.Request{
+				Method: http.MethodGet,
+				Host:   "example.com",
+				URL: &url.URL{
+					Host:     "127.0.0.1",
+					Path:     "/foo",
+					RawQuery: "a=1&b=2",
+				},
+			},
+			nil,
+			"get|example.com|127.0.0.1|/foo|a=1&b=2",
+			false,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			got, err := Seed(tt.req, tt.vary)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("got Seed() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("got Seed() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestEncodeAndDecode(t *testing.T) {
 	tests := []struct {
 		req     *http.Request
